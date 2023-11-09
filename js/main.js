2922b6c4f78da8d1a04e2cb47547cd1e14bf334f
Kai <dungnt07112012@gmail.com>
2023-11-09 18:12:12 +0900

    Implement item 3, not include item 4

--- js/main.js
+++ js/main.js
@@ -1,26 +1,26 @@
 var btnResult = document.getElementById("btnResult");
 btnResult.onclick = function () {
 
-    var diemChuan = document.getElementById("diemChuan").value * 1;
-    var diemToan = document.getElementById("diemToan").value * 1;
-    var diemLy = document.getElementById("diemLy").value * 1;
-    var diemHoa = document.getElementById("diemHoa").value * 1;
-    var khuVuc = document.getElementById("khuVuc").value * 1;
-    var doiTuong = document.getElementById("doiTuong").value * 1;
-    var tongDiem = 0;
+  var diemChuan = document.getElementById("diemChuan").value * 1;
+  var diemToan = document.getElementById("diemToan").value * 1;
+  var diemLy = document.getElementById("diemLy").value * 1;
+  var diemHoa = document.getElementById("diemHoa").value * 1;
+  var khuVuc = document.getElementById("khuVuc").value * 1;
+  var doiTuong = document.getElementById("doiTuong").value * 1;
+  var tongDiem = 0;
 
-    tongDiem = diemToan + diemLy + diemHoa + khuVuc + doiTuong;
+  tongDiem = diemToan + diemLy + diemHoa + khuVuc + doiTuong;
 
-    if (diemToan == 0 || diemLy == 0 || diemHoa == 0) {
-        document.getElementById("diemDatDuoc").innerHTML = "Tổng điểm : " + tongDiem;
-        document.getElementById("result").innerHTML = "Kết quả : Trượt " + "(Do có môn bị điểm 0)";
-    } else if (tongDiem >= diemChuan) {
-        document.getElementById("diemDatDuoc").innerHTML = "Tổng điểm : " + tongDiem;
-        document.getElementById("result").innerHTML = "Kết quả : Đỗ";
-    } else {
-        document.getElementById("diemDatDuoc").innerHTML = "Tổng điểm : " + tongDiem;
-        document.getElementById("result").innerHTML = "Kết quả : Trượt";
-    }
+  if (diemToan == 0 || diemLy == 0 || diemHoa == 0) {
+    document.getElementById("diemDatDuoc").innerHTML = "Tổng điểm : " + tongDiem;
+    document.getElementById("result").innerHTML = "Kết quả : Trượt " + "(Do có môn bị điểm 0)";
+  } else if (tongDiem >= diemChuan) {
+    document.getElementById("diemDatDuoc").innerHTML = "Tổng điểm : " + tongDiem;
+    document.getElementById("result").innerHTML = "Kết quả : Đỗ";
+  } else {
+    document.getElementById("diemDatDuoc").innerHTML = "Tổng điểm : " + tongDiem;
+    document.getElementById("result").innerHTML = "Kết quả : Trượt";
+  }
 
 }
 
@@ -56,25 +56,97 @@ Tính tiền Điện
 
 var btnTienDien = document.getElementById("btnTienDien");
 btnTienDien.onclick = function () {
-    var chuHo = document.getElementById("chuHo").value ;
-    var soDien = document.getElementById("soDien").value * 1;
-    var tienDien = 0 ;
+  var chuHo = document.getElementById("chuHo").value;
+  var soDien = document.getElementById("soDien").value * 1;
+  var tienDien = 0;
 
-    if (soDien <= 50) {
-        tienDien = soDien * 500 ;
-      } else if ( 50 < soDien && soDien <= 100) {
-        tienDien = 50 * 500 + (soDien - 50) * 650 ;
-      } else if ( 100 < soDien && soDien <= 200) {
-        tienDien = 50 * 500 + 50 * 650 + (soDien - 100) * 850 ;
-      }  else if ( 200 < soDien && soDien <= 350) {
-        tienDien = 50 * 500 + 50 * 650 + 100 * 850 + (soDien - 200) * 1100 ;
-      } else {
-        tienDien = 50 * 500 + 50 * 650 + 100 * 850 + 200 * 1100 + (soDien - 350) * 1300 ;
-      }
+  if (soDien <= 50) {
+    tienDien = soDien * 500;
+  } else if (50 < soDien && soDien <= 100) {
+    tienDien = 50 * 500 + (soDien - 50) * 650;
+  } else if (100 < soDien && soDien <= 200) {
+    tienDien = 50 * 500 + 50 * 650 + (soDien - 100) * 850;
+  } else if (200 < soDien && soDien <= 350) {
+    tienDien = 50 * 500 + 50 * 650 + 100 * 850 + (soDien - 200) * 1100;
+  } else {
+    tienDien = 50 * 500 + 50 * 650 + 100 * 850 + 200 * 1100 + (soDien - 350) * 1300;
+  }
 
-      const config = { style: "currency", currency: "VND" };
-      const formated = new Intl.NumberFormat("vi-VN", config);
-  
-      document.getElementById("tienDien").innerHTML = "Tiền điện của nhà " + chuHo + " là : " + formated.format(tienDien);
+  const config = { style: "currency", currency: "VND" };
+  const formated = new Intl.NumberFormat("vi-VN", config);
 
+  document.getElementById("tienDien").innerHTML = "Tiền điện của nhà " + chuHo + " là : " + formated.format(tienDien);
+
+}
+
+/*
+Chương trình tính thuế thu nhập
+■ Input
+- Nhập thông tin cá nhân
+  + Họ tên
+  + Tổng thu nhập năm
+  + Số người phụ thuộc
+■ Process
+- Tạo biến
+  + btnThue
+  + hoTen
+  + thuNhap
+  + phuThuoc
+  + thuNhapChiuThue
+  + tienThue
+
+- Xử lý
+  thuNhapChiuThue = thuNhap - 4 - phuThuoc * 1.6 ;
+
+  if (thuNhapChiuThue <= 60) {
+    tienThue = thuNhapChiuThue * 0.05 ;
+  } else if (60 < thuNhapChiuThue && thuNhapChiuThue <= 120) {
+    tienThue = thuNhapChiuThue * 0.1 ;
+  } else if (120 < thuNhapChiuThue && thuNhapChiuThue <= 210) {
+    tienThue = thuNhapChiuThue * 0.15 ;
+  } else if (210 < thuNhapChiuThue && thuNhapChiuThue <= 384) {
+    tienThue = thuNhapChiuThue * 0.2 ;
+  } else if (384 < thuNhapChiuThue && thuNhapChiuThue <= 624) {
+    tienThue = thuNhapChiuThue * 0.25 ;
+  } else if (624 < thuNhapChiuThue && thuNhapChiuThue <= 960) {
+    tienThue = thuNhapChiuThue * 0.3 ;
+  } else {
+    tienThue = thuNhapChiuThue * 0.35 ;
+  }
+
+- Xuất kết quả
+  + ketQua.innerHTML = 
+
+■ Output
+- Tính thuế thu nhập cá nhân phải trả theo quy định
+*/
+
+var btnThue = document.getElementById("btnThue");
+btnThue.onclick = function () {
+  var hoTen = document.getElementById("hoTen").value ;
+  var thuNhap = document.getElementById("thuNhap").value * 1;
+  var phuThuoc = document.getElementById("phuThuoc").value * 1;
+  var thuNhapChiuThue = 0;
+  var tienThue = 0;
+
+  thuNhapChiuThue = thuNhap - 4 - phuThuoc * 1.6 ;
+
+  if (thuNhapChiuThue <= 60) {
+    tienThue = thuNhapChiuThue * 0.05 ;
+  } else if (60 < thuNhapChiuThue && thuNhapChiuThue <= 120) {
+    tienThue = thuNhapChiuThue * 0.1 ;
+  } else if (120 < thuNhapChiuThue && thuNhapChiuThue <= 210) {
+    tienThue = thuNhapChiuThue * 0.15 ;
+  } else if (210 < thuNhapChiuThue && thuNhapChiuThue <= 384) {
+    tienThue = thuNhapChiuThue * 0.2 ;
+  } else if (384 < thuNhapChiuThue && thuNhapChiuThue <= 624) {
+    tienThue = thuNhapChiuThue * 0.25 ;
+  } else if (624 < thuNhapChiuThue && thuNhapChiuThue <= 960) {
+    tienThue = thuNhapChiuThue * 0.3 ;
+  } else {
+    tienThue = thuNhapChiuThue * 0.35 ;
+  }
+
+  document.getElementById("thuNhapChiuThue").innerHTML = "Thu nhập chịu thuế là : " + thuNhapChiuThue + " (triệu đồng)";
+  document.getElementById("tienThue").innerHTML = "Tiền thuế cần nộp là : " + tienThue.toFixed(2) + " (triệu đồng)";
 }
